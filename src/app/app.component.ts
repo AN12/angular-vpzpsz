d21bca9939a6ee4d9a6a483228bf7d4f5d451c7c
AN12 <ananya.nadendla@mcvts.org>
2020-02-28 10:02:48 -0500

    comment added - testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {}
-
+//I added a comment here for testing purposes
 
 /*
 Copyright Google LLC. All Rights Reserved.
